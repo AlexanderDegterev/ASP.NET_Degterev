8a2bb4e1caf894bd4bff04fa476bfffb45a1fd72
AlexanderDegterev <Alexander.1@mail.ru>
2015-07-19 00:07:51 +0300

    18/07/2015 Web

---
+++ Statistics_of_sales/StatisticsOfSales.WebUI/Controllers/SaleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StatisticsOfSales.Domain.Abstract;
+using StatisticsOfSales.Domain.Entities;
+
+namespace StatisticsOfSales.WebUI.Controllers
+{
+    public class SaleController : Controller
+    {
+         private ISaleRepository repository; 
+         public SaleController(ISaleRepository saleRepository) 
+    {
+        this.repository = saleRepository; 
+    }
+
+         public ViewResult List()
+         {
+             return View(repository.Sales);
+         } 
+
+    }
+}
