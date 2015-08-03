6d5c366526db1cfaeae1ffd3dc6890f6301025d2
AlexanderDegterev <Alexander.1@mail.ru>
2015-08-03 23:30:19 +0300

    v2 and v4

--- StatisticsOfSales_v2/StatisticsOfSales_v2/Controllers/HomeController.cs
+++ StatisticsOfSales_v2/StatisticsOfSales_v2/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace StatisticsOfSales_v2.Controllers
         {
             return View();
         }
-
+        [Authorize]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
+        [Authorize(Roles = "admin")]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
@@ -52,32 +52,9 @@ namespace StatisticsOfSales_v2.Controllers
 
         public JsonResult GetManagerAgePie()
         {
-            /*var salesManager = new List<object>();
-
-            salesManager.Add(new { Title = "Ghostbusters", Genre = "Comedy", Year = 1984 });
-            salesManager.Add(new { Title = "Gone with Wind", Genre = "Drama", Year = 1939 });
-            salesManager.Add(new { Title = "Star Wars", Genre = "Science Fiction", Year = 1977 });*/
-            /*List<ResultLine> result = db.Sales
-                .GroupBy(l => l.Manager.ManagerName)
-                .Select(cl => new ResultLine
-            {
-                ManagerName = cl.First().Manager.ManagerName.ToString(),
-                SumTotal = cl.Sum(c => c.TotalSum).ToString(),
-            }).ToList();*/
-
-            var groupedData = from b in db.Sales.AsEnumerable()
-                              group b by b.ManagerID into g
-                              select new
-                              {
-                                  manager = g.Key,
-                                  amount = g.Sum(b => b.TotalSum).ToString()
-                              };
-
             var salesManager2 = (from s in db.Sales
                 group s by s.Manager.ManagerName into g
                 select new { amount = g.Sum(x => x.TotalSum), manager = g.Key }).ToList(); 
-            //(from x in db.Sales group x by x.ManagerID).ToList();
-            //ViewBag.SM2 = salesManager2;
 
             return Json(new { Salemanager = salesManager2 }, JsonRequestBehavior.AllowGet);
         }
